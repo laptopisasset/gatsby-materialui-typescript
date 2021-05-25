a1d4f1bd467137cef3a3124f223b4c363ddb4e5e
Soundhar <laptopisasset@gmail.com>
2021-05-25 08:16:11 +0530

    added eleveation.

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,11 +1,24 @@
-import React from "react"
+import React, { FC, ReactElement } from "react"
 
-import { AppBar, Toolbar } from "@material-ui/core"
+import { AppBar, Toolbar, useScrollTrigger } from "@material-ui/core"
 
-export const Header = () => {
+const ElevationScroll: FC = ({ children }) => {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  })
+
+  return React.cloneElement(children as ReactElement, {
+    elevation: trigger ? 4 : 0,
+  })
+}
+
+export const Header: FC = () => {
   return (
-    <AppBar>
-      <Toolbar>Arc Development</Toolbar>
-    </AppBar>
+    <ElevationScroll>
+      <AppBar>
+        <Toolbar>Arc Development</Toolbar>
+      </AppBar>
+    </ElevationScroll>
   )
 }
