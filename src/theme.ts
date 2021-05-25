abe766cc0151ab76f8df4ab034a986fa16c8fe5b
Soundhar <laptopisasset@gmail.com>
2021-05-25 09:56:59 +0530

    Theme customization started.

--- src/theme.ts
+++ src/theme.ts
@@ -1,14 +1,34 @@
 import { red } from "@material-ui/core/colors"
 import { createMuiTheme } from "@material-ui/core/styles"
 
+/**
+ * Type Augumentation for custom variables
+ */
+declare module "@material-ui/core/styles/createPalette" {
+  interface CommonColors {
+    blue: string
+    orange: string
+  }
+}
+
+/**
+ * Theme Constants
+ */
+const blue = "#0B72B9"
+const orange = "#FFBA60"
+
 // A custom theme for this app
 const theme = createMuiTheme({
   palette: {
+    common: {
+      blue,
+      orange,
+    },
     primary: {
-      main: "#556cd6",
+      main: blue,
     },
     secondary: {
-      main: "#19857b",
+      main: orange,
     },
     error: {
       main: red.A400,
