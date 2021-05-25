f7a6410ac30f8397cde7633c4fdf526452fb819a
Soundhar <laptopisasset@gmail.com>
2021-05-25 09:36:07 +0530

    Back to Home.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,5 @@ import React from "react"
 import { NormalLayout } from "../components/layouts"
 
 export default function Index() {
-  return (
-    <NormalLayout>
-      {[...new Array(120)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join("\n")}
-    </NormalLayout>
-  )
+  return <NormalLayout>Hello</NormalLayout>
 }
