5c74ff3062d00ab900172afa808a032425cce3fe
Soundhar <laptopisasset@gmail.com>
2021-05-25 07:58:10 +0530

    Added the code for index.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,6 @@
 import React from "react"
-import Container from "@material-ui/core/Container"
-import Typography from "@material-ui/core/Typography"
-import Box from "@material-ui/core/Box"
-
-import { Link, Copyright } from "../components/ui"
+import { NormalLayout } from "../components/layouts"
 
 export default function Index() {
-  return (
-    <Container maxWidth="lg">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Gatsby v4-beta example
-        </Typography>
-        <Link to="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <Copyright />
-      </Box>
-    </Container>
-  )
+  return <NormalLayout></NormalLayout>
 }
