ccb6b217dedc748a57e3b5e677488be388b501fa
Soundhar Natarajan <soundhar.n@aximsoft.com>
2021-05-25 13:05:20 +0530

    Added gatsby config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,16 @@ module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
     "gatsby-plugin-top-layout",
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: "gatsby-plugin-material-ui",
       // If you want to use styled components you should change the injection order.
